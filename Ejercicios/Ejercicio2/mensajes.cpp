d3b8eb94d3ac52f5e7bd9056f76d7145f77126e9
Eli Vallejo <ovallejo@unah.hn>
2021-06-06 22:21:25 -0600

    agregando ejercicio2

---
+++ Ejercicios/Ejercicio2/mensajes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   string nombre;
+    
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+
+    cout << endl;
+    cout << "hola mi nombre es " << nombre;
+    cout << endl;
+
+    return 0;
+}
