a92e93c259896412f25cd158649ccfd31faf3fb0
Eli Vallejo <ovallejo@unah.hn>
2021-06-07 20:17:41 -0600

    creando condicion

---
+++ Ejercicios/condicionesIF/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 5;
+    int b = 7;
+    int c = 0;
+
+    bool cierto = a > b;
+
+
+
+    if (a > b);
+
+    {
+     /* code */
+    } else {
+
+    }
+
+    if (a > b && a >c)
+    {
+
+    }
+    else 
+    {
+
+    }
+
+    if (a > b  a >c)
+    {
+
+    }
+    else 
+    {
+
+    }
+
+
+
+    return 0;
+}
